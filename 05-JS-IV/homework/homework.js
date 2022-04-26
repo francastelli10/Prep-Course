5b799f23749dfecff5171b18b7704fb2150d3759
francastelli10 <jrr10.fc@gmail.com>
2022-04-26 19:00:05 -0300

    commiteando

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,8 +6,15 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var obj = {
+    nombre: nombre,
+    edad: edad,
+    meow: function () {
+      return "Meow!";
+  }
 }
-
+return objeto;
+}
 
 function agregarPropiedad(objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
